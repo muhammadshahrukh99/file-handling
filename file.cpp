3eb1d10bea47ad9a6e33933720b71cbed720a2bb
shahrukh <muhammadshahrukh99@gmail.com>
2020-08-05 15:21:47 +0500

    multi

--- file.cpp
+++ file.cpp
@@ -5,7 +5,7 @@ int main(){
 
 int num[10]; 
 int sum=0 ;
-int mul=0;
+int mul=1;
 for (int i = 0; i < 10; i++)
 {
     cin>>num[i];
@@ -16,7 +16,14 @@ for (int i = 0; i < 10; i++)
     sum=sum+num[i];
     
 }
-cout<<"sum of numbers"<<sum;
+
+
+for (int i = 0; i < 10; i++)
+{
+    mul=mul*num[i];
+}
+cout<<"sum of numbers : "<<sum<<endl;
+cout<<"Multi of numbers : "<<mul<<endl;
 
 return 0; 
 }
