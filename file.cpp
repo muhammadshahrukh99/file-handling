6bb247f763a0a609eeb07cd20545a31a98740b40
shahrukh <muhammadshahrukh99@gmail.com>
2020-08-05 15:04:13 +0500

    numbers in arry

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+
+int num[10]; 
+int sum=0 ;
+int mul=0;
+for (int i = 0; i < 10; i++)
+{
+    cin>>num[i];
+
+}
+
+
+return 0; 
+}
